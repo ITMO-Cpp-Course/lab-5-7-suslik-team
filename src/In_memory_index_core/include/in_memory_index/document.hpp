630d145a6aaa027f7f5ecf5f0e357d1e04444433
Igor <garrykarlik@gmail.com>
2026-04-29 01:24:32 +0300

    add document class

--- src/In_memory_index_core/include/in_memory_index/document.hpp
+++ src/In_memory_index_core/include/in_memory_index/document.hpp
@@ -1,10 +1,27 @@
 #pragma once
 
-
+#include <cstdint>
+#include <string>
+#include <string_view>
 
 namespace in_memory_index
 {
 
+struct Document
+{
+    std::uint64_t id = 0;
+    std::string name;
+    std::string content;
 
+    Document() = default;
+    Document(std::uint64_t id, std::string name, std::string content);
+
+    Document(const Document& other) = default;
+    Document(Document&& other) noexcept;
+    Document& operator=(const Document& other) = default;
+    Document& operator=(Document&& other) noexcept;
+
+    std::string_view text() const noexcept;
+};
 
 }
--- src/In_memory_index_core/src/document.cpp
+++ src/In_memory_index_core/src/document.cpp
@@ -3,4 +3,30 @@
 namespace in_memory_index
 {
 
+Document::Document(std::uint64_t doc_id, std::string doc_name, std::string doc_content)
+    : id(doc_id), name(std::move(doc_name)), content(std::move(doc_content))
+{
+}
+
+Document::Document(Document&& other) noexcept
+    : id(other.id), name(std::move(other.name)), content(std::move(other.content))
+{
+}
+
+Document& Document::operator=(Document&& other) noexcept
+{
+    if (this != &other)
+    {
+        id = other.id;
+        name = std::move(other.name);
+        content = std::move(other.content);
+    }
+    return *this;
+}
+
+std::string_view Document::text() const noexcept
+{
+    return content;
+}
+
 }
