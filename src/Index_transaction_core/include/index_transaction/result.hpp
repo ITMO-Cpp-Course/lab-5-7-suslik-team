454460291dc1b44df02b1f27322d5cfd1d0d2d48
Danil <uryadnikov.07@mail.ru>
2026-05-07 15:17:04 +0300

    delete space

---
+++ src/Index_transaction_core/include/index_transaction/result.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <expected>
+#include <string>
+#include <utility> 
+
+namespace index_transaction{
+    template <typename T>
+    class Result{
+        public:
+
+        private:
+        
+    }
+}
